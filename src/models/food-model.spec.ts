efb13b96b71556f696a055fe583ebbe84a58bbb8
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-06-24 09:59:54 +0200

    refactor: refactor class foodModel

--- src/models/food-model.spec.ts
+++ src/models/food-model.spec.ts
@@ -2,7 +2,11 @@ import { Food } from './food-model'
 
 describe('Classes', () => {
   it('Class FoodModel', () => {
-    const food = new Food('name', 'description', 50)
+    const food = new Food({
+      name: 'name',
+      description: 'description',
+      price: 50
+    })
 
     expect(food.getName()).toEqual('name')
     expect(food.getDescription()).toEqual('description')
